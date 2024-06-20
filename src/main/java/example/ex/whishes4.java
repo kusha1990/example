af40b692f95b50f7cc4c296b5781271cc3a8b130
kusha1990 <cg.kusha@gmail.com>
2024-06-20 15:00:00 +0530

    whishes4

---
+++ src/main/java/example/ex/whishes4.java
@@ -0,0 +1,10 @@
+package example.ex;
+
+public class whishes4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("whishes from javaclas");
+	}
+
+}
