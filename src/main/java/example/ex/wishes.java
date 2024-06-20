4f32442d1cd8557aa93552051a8a7ad5dce222f2
kusha1990 <cg.kusha@gmail.com>
2024-06-20 14:09:11 +0530

    new file updated

---
+++ src/main/java/example/ex/wishes.java
@@ -0,0 +1,10 @@
+package example.ex;
+
+public class wishes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("good morning from eclipse");
+	}
+
+}
