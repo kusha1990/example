3ceed00332838e8dd05c9fc29bc6ffb3dd4a4258
kusha1990 <cg.kusha@gmail.com>
2024-06-20 14:17:23 +0530

    "third wish from eclipse"

---
+++ src/main/java/example/ex/wishes3.java
@@ -0,0 +1,10 @@
+package example.ex;
+
+public class wishes3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("whishes from eclipse");
+	}
+
+}
